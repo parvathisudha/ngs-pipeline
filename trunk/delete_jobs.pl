d5fe3f0a2a9f9305f5c9703657e0d5af65679bc8
reshetovdenis <reshetovdenis@6417c1ee-9a8a-29c9-8368-fd6fd0c9e1d9>
2011-04-11 18:45:41 +0000

    Stable release.

--- trunk/delete_jobs.pl
+++ trunk/delete_jobs.pl
@@ -9,4 +9,4 @@ my $config = ConfigRun->new( $ARGV[0] );
 my $project = Project->new( $config, 0 );
 my $ids = $project->get_all_ids();
 my $cmd = join(",", @$ids);
-system("qdel $cmd");
+system("qdel -f $cmd");

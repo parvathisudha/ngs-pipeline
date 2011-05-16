04e4fe85329648031ac9a20b433c7917a384bcf9
reshetovdenis <reshetovdenis@6417c1ee-9a8a-29c9-8368-fd6fd0c9e1d9>
2011-05-16 09:34:10 +0000

    Added test support for variant annotatiom (from GATK), indels calling, variant filtration and recalibration

--- trunk/ngs-pipeline.pl
+++ trunk/ngs-pipeline.pl
@@ -33,20 +33,20 @@ my $sampe  = "$bwa sampe";
 my $samse  = "$bwa samse";
 
 #samtools commands
-my $samtools  = $config->{'SAMTOOLS'} . "/samtools";
-my $import    = "$samtools import";
-my $sort      = "$samtools sort";
-my $index     = "$samtools index";
-my $merge     = "$samtools merge";
-my $view      = "$samtools view";
-my $gatk      = $config->{'GATK'} . "/GenomeAnalysisTK.jar";
-my $mark_dup      = $project->{'CONFIG'}->{'PICARD'} . '/' . "MarkDuplicates.jar";
-my $picard_sort_index      = $project->{'CONFIG'}->{'PICARD'} . '/' . "SortSam.jar";
-my $picard_index      = $project->{'CONFIG'}->{'PICARD'} . '/' . "BuildBamIndex.jar";
-my $call      = "";
-my $genome    = $config->{'GENOME'};
-my $gene_list = $config->{'GENELIST'};
-my $effect    =
+my $samtools = $config->{'SAMTOOLS'} . "/samtools";
+my $import   = "$samtools import";
+my $sort     = "$samtools sort";
+my $index    = "$samtools index";
+my $merge    = "$samtools merge";
+my $view     = "$samtools view";
+my $gatk     = $config->{'GATK'} . "/GenomeAnalysisTK.jar";
+my $mark_dup = $project->{'CONFIG'}->{'PICARD'} . '/' . "MarkDuplicates.jar";
+my $picard_sort_index = $project->{'CONFIG'}->{'PICARD'} . '/' . "SortSam.jar";
+my $picard_index = $project->{'CONFIG'}->{'PICARD'} . '/' . "BuildBamIndex.jar";
+my $call         = "";
+my $genome       = $config->{'GENOME'};
+my $gene_list    = $config->{'GENELIST'};
+my $effect       =
   $config->{'VCFCODINGSNPS'} . "/vcfCodingSnps.v1.5 -r $genome -g $gene_list";
 my $filter_interesting = "perl /data/software/filter_interesting.pl";
 my $genome_coverage    = $config->{'BEDTOOLS'} . "/genomeCoverageBed";
@@ -60,42 +60,48 @@ my $BreakDancerMini  = "perl $break_dancer_dir/BreakDancerMini.pl";
 #define_done_jobs($project);
 
 for my $lane ( @{ $config->{LANES} } ) {
-	align( $project, $lane );
-	sai_to_sam( $project, $lane );
+	align( $project, $lane );#tested
+	sai_to_sam( $project, $lane );#tested
 
-	#import_sam( $project, $lane );
-	sort_bam( $project, $lane );
-	index_bam( $project, $lane );
+	#import_sam( $project, $lane );#to deletion
+	sort_bam( $project, $lane );#rewrite with picard
+	index_bam( $project, $lane );#rewrite with picard
 
 }
-merge_bams($project);
+merge_bams($project);#rewrite with picard
 
-#sort_merged($project);
-index_merged($project);
-mark_duplicates($project);
-realigner_target_creator($project);
-indel_realigner($project);
-index_realigned($project);
-count_covariates($project);
-table_recalibration($project);
-index_recalibrated($project);
-parallel_call_SNPs($project);
-parallel_predict_effect($project);
-merge_vcf($project);
-variant_recalibrator($project);
-apply_recalibration($project);
+#sort_merged($project);#to deletion
+index_merged($project);#tested
+mark_duplicates($project);#tested
+realigner_target_creator($project);#tested
+indel_realigner($project);#tested
+index_realigned($project);#tested
+count_covariates($project);#tested
+table_recalibration($project);#tested
+index_recalibrated($project);#tested
+parallel_call_SNPs($project);#!!
+parallel_call_indels($project);#!!
+parallel_predict_effect($project);#!!
+parallel_predict_indels_effect($project);#!!
+#merge_vcf($project);#method to deletion
+merge_snps($project);#!!
+merge_indels($project);#!!
+sub a{}
+variant_annotator($project);#!!
+filter_snps($project);#!!
+variant_recalibrator($project);#!!
+apply_recalibration($project);#!!
+callable_loci($project);#rewrite to use recalibrated bam
 
-callable_loci($project);
-
-#depth_coverage($project);
-calculate_genome_coverage($project);
-calculate_bga_coverage($project);
+#depth_coverage($project);#rewrite to use recalibrated bam
+calculate_genome_coverage($project);#rewrite to use recalibrated bam
+calculate_bga_coverage($project);#rewrite to use recalibrated bam
 
 bgzip($project);
 tabix($project);
-breakdancer_cfg($project);
-breakdancer_mini($project);
-breakdancer_max($project);
+#breakdancer_cfg($project);#install on cluster
+#breakdancer_mini($project);#install on cluster
+#breakdancer_max($project);#install on cluster
 
 #clean($project);
 
@@ -334,13 +340,13 @@ PROGRAM
 }
 
 sub index_realigned {
-	my ( $project ) = @_;
+	my ($project) = @_;
 	my $sorted = $project->index_realigned;
 	return 1 if ( -e $sorted );
 	sleep($sleep_time);
 	my $indel_realigned = $project->indel_realigner();
-	my $tmp_dir = $project->dir;
-	my $program = <<PROGRAM;
+	my $tmp_dir         = $project->dir;
+	my $program         = <<PROGRAM;
 java -Xmx10g -jar $picard_sort_index \\
 INPUT=$indel_realigned \\
 OUTPUT=$sorted \\
@@ -352,7 +358,8 @@ MAX_RECORDS_IN_RAM=2500000
 PROGRAM
 	my $qsub_param =
 	  '-hold_jid ' . $project->task_id( $project->indel_realigner_id );
-	$task_scheduler->submit( $project->index_realigned_id, $qsub_param, $program );
+	$task_scheduler->submit( $project->index_realigned_id,
+		$qsub_param, $program );
 }
 
 sub count_covariates {
@@ -410,8 +417,8 @@ sub index_recalibrated {
 	sleep($sleep_time);
 	return 1 if ( -e $index_recal );
 	my $bam_recal = $project->table_recalibration();
-	my $tmp_dir = $project->dir;
-	my $program = <<PROGRAM;
+	my $tmp_dir   = $project->dir;
+	my $program   = <<PROGRAM;
 java -jar $picard_index \\
 INPUT=$bam_recal \\
 OUTPUT=$index_recal \\
@@ -426,31 +433,65 @@ PROGRAM
 
 sub parallel_call_SNPs {
 	my ($project) = @_;
-	my @chr = $project->read_intervals();
+	my @chr       = $project->read_intervals();
 	my $bam_recal = $project->table_recalibration();
 	for my $chr (@chr) {
 		sleep($sleep_time);
 		my $recal_file = $project->count_covariates();
 		my $gatk_vcf   = $project->parallel_gatk_vcf($chr);
 		next if ( -e $gatk_vcf );
-		my $id      = $project->{'CONFIG'}->{'PROJECT'};
-		my $dbSNP   = $project->{'CONFIG'}->{'DBSNP'};
-		my $program = <<PROGRAM;
+		my $id              = $project->{'CONFIG'}->{'PROJECT'};
+		my $dbSNP           = $project->{'CONFIG'}->{'DBSNP'};
+		my $stand_call_conf = $project->{'CONFIG'}->{'GATK_stand_call_conf'};
+		my $stand_emit_conf = $project->{'CONFIG'}->{'GATK_stand_emit_conf'};
+		my $program         = <<PROGRAM;
 java -Xmx4g -jar $gatk \\
 -R $genome \\
 -T UnifiedGenotyper \\
 -I $bam_recal \\
 -B:dbsnp,VCF $dbSNP \\
 -o $gatk_vcf \\
--stand_call_conf 20.0 \\
--stand_emit_conf 10.0 \\
+-stand_call_conf $stand_call_conf \\
+-stand_emit_conf $stand_emit_conf \\
 -dcov 80 -U \\
 -L $chr
 PROGRAM
 		my $qsub_param =
-		  '-hold_jid '
-		  . $project->task_id( $project->index_recalibrated_id() );
+		  '-hold_jid ' . $project->task_id( $project->index_recalibrated_id() );
 		$task_scheduler->submit( $project->parallel_gatk_vcf_id($chr),
+			$qsub_param, $program );
+	}
+}
+
+sub parallel_call_indels {
+	my ($project) = @_;
+	my @chr       = $project->read_intervals();
+	my $bam_recal = $project->table_recalibration();
+	for my $chr (@chr) {
+		sleep($sleep_time);
+		my $recal_file = $project->count_covariates();
+		my $gatk_vcf   = $project->parallel_call_indels($chr);
+		next if ( -e $gatk_vcf );
+		my $id              = $project->{'CONFIG'}->{'PROJECT'};
+		my $dbSNP           = $project->{'CONFIG'}->{'DBSNP'};
+		my $stand_call_conf = $project->{'CONFIG'}->{'GATK_stand_call_conf'};
+		my $stand_emit_conf = $project->{'CONFIG'}->{'GATK_stand_emit_conf'};
+		my $program         = <<PROGRAM;
+java -Xmx4g -jar $gatk \\
+-R $genome \\
+-T UnifiedGenotyper \\
+-glm DINDEL \\
+-I $bam_recal \\
+-B:dbsnp,VCF $dbSNP \\
+-o $gatk_vcf \\
+-stand_call_conf $stand_call_conf \\
+-stand_emit_conf $stand_emit_conf \\
+-dcov 80 -U \\
+-L $chr
+PROGRAM
+		my $qsub_param =
+		  '-hold_jid ' . $project->task_id( $project->index_recalibrated_id() );
+		$task_scheduler->submit( $project->parallel_call_indels_id($chr),
 			$qsub_param, $program );
 	}
 }
@@ -460,8 +501,8 @@ sub parallel_predict_effect {
 	my @chr = $project->read_intervals();
 	for my $chr (@chr) {
 		sleep($sleep_time);
-		my $eff_vcf    = $project->parallel_predict_effect($chr);
-		my $gatk_vcf   = $project->parallel_gatk_vcf($chr);
+		my $eff_vcf  = $project->parallel_predict_effect($chr);
+		my $gatk_vcf = $project->parallel_gatk_vcf($chr);
 		next if ( -e $eff_vcf );
 		my $program    = "$effect -s $gatk_vcf -o $eff_vcf -l $eff_vcf.log";
 		my $qsub_param =
@@ -472,17 +513,31 @@ sub parallel_predict_effect {
 	}
 }
 
-
-
-
-sub merge_vcf {
+sub parallel_predict_indels_effect {
 	my ($project) = @_;
-	my $merged_vcf = $project->merge_vcf();
+	my @chr = $project->read_intervals();
+	for my $chr (@chr) {
+		sleep($sleep_time);
+		my $eff_vcf  = $project->parallel_predict_indels_effect($chr);#
+		my $gatk_vcf = $project->parallel_call_indels($chr);
+		next if ( -e $eff_vcf );
+		my $program    = "$effect -s $gatk_vcf -o $eff_vcf -l $eff_vcf.log";
+		my $qsub_param =
+		  '-hold_jid '
+		  . $project->task_id( $project->parallel_call_indels_id($chr) );
+		$task_scheduler->submit( $project->parallel_predict_indels_effect_id($chr),
+			$qsub_param, $program );
+	}
+}
+
+sub merge_parallel_vcf {
+	my ( $project, $vcfs, $merged_name, $after_id, $id ) = @_;
+	my $merged_vcf = $merged_name;
 	return 1 if ( -e $merged_vcf );
 	sleep($sleep_time);
-	my @vcfs = map { "-B:sample,VCF $_" } $project->all_gatk_vcf();
-	my $all_vcf = join(' ', @vcfs);
-	my $program         = <<PROGRAM;
+	my @vcfs = map { "-B:sample,VCF $_" } @$vcfs;
+	my $all_vcf = join( ' ', @vcfs );
+	my $program = <<PROGRAM;
 java -Xmx4g -jar $gatk \\
 -T CombineVariants \\
 -R $genome \\
@@ -491,10 +546,50 @@ $all_vcf \\
 -variantMergeOptions UNION \\
 --assumeIdenticalSamples
 PROGRAM
-	my $qsub_param =
-	  '-hold_jid ' . $project->all_annotated();
-	$task_scheduler->submit( $project->merge_vcf_id(),
-		$qsub_param, $program );
+	my $qsub_param = '-hold_jid ' . $after_id;
+	$task_scheduler->submit( $id, $qsub_param, $program );
+}
+
+sub merge_snps {
+	my ($project) = @_;
+	my $merged_vcf = $project->merge_vcf;
+	merge_parallel_vcf(
+		$project,            $project->all_snps_eff_vcf,
+		$project->merge_vcf, $project->all_annotated,
+		$project->merge_vcf_id
+	);
+}
+
+sub merge_indels {
+	my ($project) = @_;
+	my $merged_vcf = $project->merge_indels;
+	merge_parallel_vcf(
+		$project,            
+		$project->all_indels_eff_vcf,
+		$project->merge_indels,
+		$project->all_indels_annotated,
+		$project->merge_indels_id
+	);
+}
+
+sub merge_vcf {
+	my ($project) = @_;
+	my $merged_vcf = $project->merge_vcf();
+	return 1 if ( -e $merged_vcf );
+	sleep($sleep_time);
+	my @vcfs = map { "-B:sample,VCF $_" } $project->all_gatk_vcf();
+	my $all_vcf = join( ' ', @vcfs );
+	my $program = <<PROGRAM;
+java -Xmx4g -jar $gatk \\
+-T CombineVariants \\
+-R $genome \\
+-o $merged_vcf \\
+$all_vcf \\
+-variantMergeOptions UNION \\
+--assumeIdenticalSamples
+PROGRAM
+	my $qsub_param = '-hold_jid ' . $project->all_annotated();
+	$task_scheduler->submit( $project->merge_vcf_id(), $qsub_param, $program );
 }
 
 sub variant_recalibrator {
@@ -502,15 +597,15 @@ sub variant_recalibrator {
 	sleep($sleep_time);
 	my $variant_recalibrator = $project->variant_recalibrator();
 	return 1 if ( -e $variant_recalibrator );
-	my $merged_vcf = $project->merge_vcf();
-	my $dbSNP   = $project->{'CONFIG'}->{'DBSNP'};
-	my $hapmap = $project->{'CONFIG'}->{'HAPMAP'};
-	my $omni = $project->{'CONFIG'}->{'OMNI'};
-	my $program = <<PROGRAM;
+	my $filtered_snps = $project->filter_snps();
+	my $dbSNP      = $project->{'CONFIG'}->{'DBSNP'};
+	my $hapmap     = $project->{'CONFIG'}->{'HAPMAP'};
+	my $omni       = $project->{'CONFIG'}->{'OMNI'};
+	my $program    = <<PROGRAM;
 java -Xmx4g -jar $gatk \\
 -T VariantRecalibrator  \\
 -R $genome \\
--B:input,VCF $merged_vcf \\
+-B:input,VCF $filtered_snps \\
 -B:hapmap,VCF,known=false,training=true,truth=true,prior=15.0 $hapmap \\
 -B:omni,VCF,known=false,training=true,truth=false,prior=12.0 $omni \\
 -B:dbsnp,VCF,known=true,training=false,truth=false,prior=8.0 $dbSNP \\
@@ -530,13 +625,13 @@ sub apply_recalibration {
 	sleep($sleep_time);
 	my $recalibrated_vcf = $project->apply_recalibration();
 	return 1 if ( -e $recalibrated_vcf );
-	my $merged_vcf = $project->merge_vcf();
+	my $filtered_snps           = $project->filter_snps();
 	my $variant_recalibrator = $project->variant_recalibrator();
-	my $program = <<PROGRAM;
+	my $program              = <<PROGRAM;
 java -Xmx3g -jar $gatk \\
 -T ApplyRecalibration  \\
 -R $genome \\
--B:input,VCF $merged_vcf \\
+-B:input,VCF $filtered_snps \\
 --ts_filter_level 99.0 \
 -recalFile $variant_recalibrator.recal \\
 -tranchesFile $variant_recalibrator \\
@@ -601,14 +696,14 @@ sub define_done_jobs {
 sub callable_loci {
 	my ($project) = @_;
 	sleep($sleep_time);
-	my $merged       = $project->merged_sorted();
+	my $bam_recal = $project->table_recalibration();
 	my $loci         = $project->callable_loci();
 	my $loci_summary = $project->callable_loci_summary();
 	my $id           = $project->{'CONFIG'}->{'PROJECT'};
 	my $genome_bed   = $project->{'CONFIG'}->{'GATKGENOMEBED'};
 	return 1 if ( -e $loci );
 	my $program = <<PROGRAM;
-java -Xmx4g -jar $gatk -R $genome -T CallableLoci -I $merged -o $loci -l INFO -format BED -maxDepth 160 -L $genome_bed -summary $loci_summary
+java -Xmx4g -jar $gatk -R $genome -T CallableLoci -I $bam_recal -o $loci -l INFO -format BED -maxDepth 160 -L $genome_bed -summary $loci_summary
 PROGRAM
 	my $qsub_param =
 	  '-hold_jid ' . $project->task_id( $project->merged_indexed_id() );
@@ -650,7 +745,8 @@ sub bgzip {
 	my $eff_vcf = $project->apply_recalibration();
 	return 1 if ( -e $project->bgzip() );
 	my $program    = "bgzip $eff_vcf";
-	my $qsub_param = '-hold_jid ' . $project->task_id( $project->apply_recalibration_id() );
+	my $qsub_param =
+	  '-hold_jid ' . $project->task_id( $project->apply_recalibration_id() );
 	$task_scheduler->submit( $project->bgzip_id(), $qsub_param, $program );
 }
 
@@ -664,26 +760,69 @@ sub tabix {
 	$task_scheduler->submit( $project->tabix_id(), $qsub_param, $program );
 }
 
+sub variant_annotator {
+	my ($project) = @_;
+	sleep($sleep_time);
+	my $annotated = $project->variant_annotator();
+	return 1 if ( -e $annotated );
+	my $snps = $project->merge_snps();
+	my $bam_recal = $project->table_recalibration();
+	my $snps_1KG = $project->{'CONFIG'}->{'1KG'};
+	my $hapmap     = $project->{'CONFIG'}->{'HAPMAP'};
+	my $omni       = $project->{'CONFIG'}->{'OMNI'};	
+	my $program = <<PROGRAM;
+java -Xmx4g -jar $gatk \\
+-T VariantAnnotator \\
+-l INFO \\
+-R $genome \\
+-I $bam_recal \
+-o $annotated \\
+--useAllAnnotations \\
+-B:variant,VCF $snps \\
+-B:comp1KG,VCF $snps_1KG \\
+-B:compHapMap,VCF $hapmap \\
+-B:compOMNI,VCF $omni \\
+PROGRAM
+	my $qsub_param =
+	  '-hold_jid ' . $project->task_id($project->merge_snps_id);
+	$task_scheduler->submit( $project->variant_annotator_id(),
+		$qsub_param, $program );
+}
+
 sub filter_snps {
 	my ($project) = @_;
 	sleep($sleep_time);
-	my $eff_vcf  = $project->eff_vcf();
 	my $filtered = $project->filter_snps();
 	return 1 if ( -e $filtered );
-	my $program    = "$filter_interesting $eff_vcf $filtered";
-	my $qsub_param = '-hold_jid ' . $project->task_id( $project->eff_vcf_id() );
-	$task_scheduler->submit( $project->filter_snps_id(), $qsub_param,
-		$program );
+	my $annotated = $project->variant_annotator();
+	my $indels = $project->merge_indels();
+	my $program = <<PROGRAM;
+java -Xmx4g -jar $gatk \\
+-T VariantFiltration \\
+-R $genome \\
+-o $filtered \\
+-B:variant,VCF $annotated \
+-B:mask,VCF $indels \
+--maskName InDel \
+--clusterWindowSize 10 \
+--filterExpression "AB < 0.2 || MQ0 > 50" \
+--filterName "20110516"
+PROGRAM
+	my $after = $project->task_id($project->variant_annotator_id) . "," . $project->task_id($project->merge_indels_id);
+	my $qsub_param =
+	  '-hold_jid ' . $after;
+	$task_scheduler->submit( $project->filter_snps_id(),
+		$qsub_param, $program );
 }
 
 sub calculate_genome_coverage {
 	my ($project) = @_;
 	sleep($sleep_time);
-	my $merged = $project->merged_sorted();
+	my $bam_recal = $project->table_recalibration();
 	return 1 if ( -e $project->genome_coverage() );
 	my $coverage_file = $project->genome_coverage();
 	my $program       =
-	    "$genome_coverage -ibam $merged -g "
+	    "$genome_coverage -ibam $bam_recal -g "
 	  . $project->{'CONFIG'}->{'BEDGENOME'}
 	  . " > $coverage_file";
 
@@ -696,12 +835,12 @@ sub calculate_genome_coverage {
 sub calculate_bga_coverage {
 	my ($project) = @_;
 	sleep($sleep_time);
-	my $merged = $project->merged_sorted();
+	my $bam_recal = $project->table_recalibration();
 	return 1 if ( -e $project->genome_coverage_bga() );
 	my $coverage_file = $project->genome_coverage_bga();
 
 	my $program =
-	    "$genome_coverage -bga -ibam $merged -g "
+	    "$genome_coverage -bga -ibam $bam_recal -g "
 	  . $project->{'CONFIG'}->{'BEDGENOME'}
 	  . " > $coverage_file";
 
@@ -742,8 +881,6 @@ sub clean() {
 	  '-hold_jid ' . $project->task_id( $project->merged_indexed_id() );
 	$task_scheduler->submit( $project->clean_id(), $qsub_param, $program );
 }
-
-
 
 sub call_SNPs {
 	my ($project) = @_;

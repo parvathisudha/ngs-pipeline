f129b38a1ea7eb9fe2a3d197f875311812e51a76
reshetovdenis <reshetovdenis@6417c1ee-9a8a-29c9-8368-fd6fd0c9e1d9>
2011-05-31 13:10:13 +0000

    Fixed problem with reading task_id

--- ngs-pipeline.pl
+++ ngs-pipeline.pl
@@ -103,7 +103,7 @@ for my $chr (@chr) {
 my @before_snps_merge;
 for my $chr (@chr) {
 	push( @before_snps_merge,
-		$project->_get_id( $project->parallel_gatk_vcf($chr) ) );
+		$project->_get_id( $project->filter_snps($chr) ) );
 }
 my $merged_snps    = $project->merge_snps();
 my $merge_snps_job = $project->merge_snps_id();

cc36b1bdab7dfca3527842f5b395254a31858fd1
reshetovdenis <reshetovdenis@6417c1ee-9a8a-29c9-8368-fd6fd0c9e1d9>
2011-12-28 14:23:01 +0000

    Added filtering support for CGI data frequencies

--- trunk/accessory/filter_vcf.pl
+++ trunk/accessory/filter_vcf.pl
@@ -1,6 +1,6 @@
 use strict;
 
-my ($in, $out, $max_kg_freq, $max_eur_freq) = @ARGV;
+my ($in, $out, $max_kg_freq, $max_eur_freq, $max_cgi_freq) = @ARGV;
 
 open IN, "<$in";
 open OUT, ">$out";
@@ -11,7 +11,8 @@ while(<IN>){
         else{
                 my $kg = $1 if m/KG_FREQ\.AF=(.+?)\;/;
                 my $eur = $1 if m/EUR_FREQ\.AF=(.+?)\;/;
-                if($kg < $max_kg_freq && $eur < $max_eur_freq){
+                my $cgi = $1 if m/CGI_FREQ\.AF=(.+?)\;/;
+                if($kg < $max_kg_freq && $eur < $max_eur_freq && $cgi < $max_cgi_freq){
                         print OUT;
                 }
         }

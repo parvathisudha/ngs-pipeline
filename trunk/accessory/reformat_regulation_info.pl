314eb2884d82a224805ecf5cfb33b78f2d3fa084
reshetovdenis <reshetovdenis@6417c1ee-9a8a-29c9-8368-fd6fd0c9e1d9>
2012-01-03 21:00:01 +0000

    Added support for reformating regulation info

--- trunk/accessory/reformat_regulation_info.pl
+++ trunk/accessory/reformat_regulation_info.pl
@@ -18,12 +18,11 @@ while(<IN>){
 	chomp;
 	my @d = split /\t/;
 	
-	my $info = $d[$eff_column];
-	my $eff = $1 if $info =~ m/EFF=(.+)(\;|$)/;
+	my $eff = $d[$eff_column];
 	my @eff = split (/,/ , $eff);
 	my @regs;
 	for my $e(@eff){
-		push (@regs, $1) if $e =~ m/REGULATION\[(.+)\]/;
+		push (@regs, $1) if $e =~ m/REGULATION\[(.+?)\]/;
 	}
 	$d[$eff_column] = join(",",@regs);
 	print join("\t",@d), "\n";

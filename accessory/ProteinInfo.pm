32fe0651465af173901cae37be307fa45c6e0cbb
reshetovdenis <reshetovdenis@6417c1ee-9a8a-29c9-8368-fd6fd0c9e1d9>
2012-01-10 10:52:20 +0000

    Fixed wrong parsing

--- accessory/ProteinInfo.pm
+++ accessory/ProteinInfo.pm
@@ -44,8 +44,6 @@ sub _download_uniprot_xml {
 	my $xml  = get 'http://www.uniprot.org/uniprot/' . $self->{id} . '.xml';
 	my $data = XMLin($xml, ForceArray => [ 'gene', 'comment' ],);
 	my $result = $data->{entry}->{comment};
-	print $self->{id}, "\n";
-	print Dumper $result;
 	push(@$result, { type => 'description', text => $data->{entry}->{protein}->{recommendedName}->{fullName} });
 	return $result;
 }

1de3d0672261acfc9cb1b3a32496505ca1e4cf04
reshetovdenis <reshetovdenis@6417c1ee-9a8a-29c9-8368-fd6fd0c9e1d9>
2012-01-12 16:37:23 +0000

    Towards set annotation

--- trunk/ngs-pipeline.pl
+++ trunk/ngs-pipeline.pl
@@ -371,7 +371,7 @@ my $regulatory_rare_table = VariantsToTable->new(
 		"-F FILTER -F EFF -F SET.set",
 		"--showFiltered"
 	],
-	previous => [$in_ensemble_regulatory]                                        #
+	previous => [$regulatory_group_annotator]                                        #
 );
 
 my $regulatory_rare_table_with_genes = JoinTabular->new(
@@ -381,9 +381,9 @@ my $regulatory_rare_table_with_genes = JoinTabular->new(
 				"--table", $regulatory_rare_table->out ,
 				"--annotation", $near_genes->out,
 				"--table_id_columns 0,1,3,4 --annotation_id_columns 0,1,3,4",
-				"--annotation_columns 13",
+				"--annotation_columns 1",
 				"--annotation_header GENE_ID",
-				"--table_columns 0,1,2,3,4,5,6,7,8,9,10,11,12",
+				"--table_columns 0,1,2,3,4,5,6,7,8,9,10,11,12,13",
 				"--skip_annotation_header",
 				"--annotation_header GENE_ID",
 				

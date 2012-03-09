13661c9463c88cad67860ee3b6a925706bf78c9e
reshetovdenis <reshetovdenis@6417c1ee-9a8a-29c9-8368-fd6fd0c9e1d9>
2012-03-09 21:12:00 +0000

    New script for reformatting Uniprot xml file

--- accessory/uniprot_xml_to_tab.pl
+++ accessory/uniprot_xml_to_tab.pl
@@ -2,76 +2,77 @@ use strict;
 use Data::Dumper;
 use XML::DOM;
 
-my $file       = $ARGV[0];
-my $dom_parser = new XML::DOM::Parser;
-my $doc        = $dom_parser->parsefile($file);
-my $root       = $doc->getDocumentElement();
-my $entries    = $doc->getElementsByTagName("entry");
-
 my @list = (
-	'accession',          'gene',
+	'accession',
+	'gene',
 	'fullName',           'function',
 	'catalytic activity', 'tissue specificity',
 	'pathway',            'disease'
 );
 print join( "\t", @list ), "\n";
-for ( my $e = 0 ; $e < $entries->getLength ; $e++ ) {
-	my $entry = $entries->item($e);
+my @files = <*xml>;
+for my $file(@files){
+	print_info($file);
+}
 
-	my $org =
-	  $entry->getElementsByTagName("organism")->item(0)
-	  ->getElementsByTagName("name")->item(0)->getFirstChild()->getData;
-	next unless $org eq 'Homo sapiens';  
-	#accession
-	my $accession =
-	  $entry->getElementsByTagName("accession")->item(0)->getFirstChild()
-	  ->getData;
 
-	#fullName
-	#uniprot->entry->protein->recommendedName->fullName
-	my $fullName =
-	  $entry->getElementsByTagName("fullName")->item(0)->getFirstChild()
-	  ->getData;
+sub print_info {
+	my ($file)     = @_;
+	my $accession = $1 if $file =~ m/\/*(.+?)\.xml/;
+	my $dom_parser = new XML::DOM::Parser;
+	my $doc        = $dom_parser->parsefile($file);
+	my $entries    = $doc->getElementsByTagName("entry");
 
-	#gene
-	#uniprot->entry->gene->name
-	my $gene =
-	  $entry->getElementsByTagName("gene")->item(0)
-	  ->getElementsByTagName("name")->item(0)->getFirstChild()->getData;
+	for ( my $e = 0 ; $e < $entries->getLength ; $e++ ) {
+		my $entry = $entries->item($e);
 
-	#All comments
-	my $comments = $entry->getElementsByTagName("comment");
-	my $types    = {
-		'function'           => [],
-		'catalytic activity' => [],
-		'tissue specificity' => [],
-		'pathway'            => [],
-		'disease'            => [],
-		'gene'               => [],
-		'fullName'           => [],
-		'accession'          => [],
-		''                   => [],
-	};
-	push( @{ $types->{'accession'} }, $accession );
-	push( @{ $types->{'gene'} },      $gene );
-	push( @{ $types->{'fullName'} },  $fullName );
-	for ( my $i = 0 ; $i < $comments->getLength ; $i++ ) {
-		my $comment = $comments->item($i);
-		my $type    = $comment->getAttribute('type');
-		my $value   = $comment->getElementsByTagName("text")->item(0);
-		if ($value) {
-			my $value_data = $value->getFirstChild()->getData;
-			push( @{ $types->{$type} }, $value_data );
+		my $org =
+		  $entry->getElementsByTagName("organism")->item(0)
+		  ->getElementsByTagName("name")->item(0)->getFirstChild()->getData;
+		next unless $org eq 'Homo sapiens';
+
+		#fullName
+		#uniprot->entry->protein->recommendedName->fullName
+		my $fullName =
+		  $entry->getElementsByTagName("fullName")->item(0)->getFirstChild()
+		  ->getData;
+
+		#gene
+		#uniprot->entry->gene->name
+		my $gene;
+		if ($entry->getElementsByTagName("gene")->item(0)) {
+			$gene = $entry->getElementsByTagName("gene")->item(0)->getElementsByTagName("name")->item(0)->getFirstChild()->getData;			
 		}
-	}
-	to_string($types);
 
-	#function
-	#uniprot->entry->comment type="function"->text
-	#'catalytic activity'
-	#'tissue specificity'
-	#'pathway',
-	#'disease'
+
+		#All comments
+		my $comments = $entry->getElementsByTagName("comment");
+		my $types    = {
+			'accession' => [],
+			'function'           => [],
+			'catalytic activity' => [],
+			'tissue specificity' => [],
+			'pathway'            => [],
+			'disease'            => [],
+			'gene'               => [],
+			'fullName'           => [],
+			''                   => [],
+		};
+		push( @{ $types->{'accession'} },      $accession );
+		push( @{ $types->{'gene'} },      $gene );
+		push( @{ $types->{'fullName'} },  $fullName );
+		for ( my $i = 0 ; $i < $comments->getLength ; $i++ ) {
+			my $comment = $comments->item($i);
+			my $type    = $comment->getAttribute('type');
+			my $value   = $comment->getElementsByTagName("text")->item(0);
+			if ($value) {
+				my $value_data = $value->getFirstChild()->getData;
+				push( @{ $types->{$type} }, $value_data );
+			}
+		}
+		to_string($types);
+	}
+
 }
 
 sub to_string {

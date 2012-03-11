162b7a9a514a4ba6e0fdfcf0e8c41164e48744b2
reshetovdenis <reshetovdenis@6417c1ee-9a8a-29c9-8368-fd6fd0c9e1d9>
2012-03-11 13:58:22 +0000

    Towards adding VEP support

--- trunk/accessory/gene_to_annotation.pl
+++ trunk/accessory/gene_to_annotation.pl
@@ -3,31 +3,32 @@ use Getopt::Long;
 use GeneAnnotator;
 use Data::Dumper;
 ####### get arguments      ###
-my ( $in, $id_column, $uniprot, $id_type, $skip_header, $uniprot_dir);
+my ( $in, $id_column, $gene_to_protein, $id_type, $uniprot_db );
 GetOptions(
-	'in=s' => \$in,
-	'id_column=s'   => \$id_column,
-	'uniprot=s'    => \$uniprot,
-	'id_type=s' => \$id_type,
-	'skip_header' => \$skip_header,
-	'uniprot_dir=s' => \$uniprot_dir,
+	'in=s'              => \$in,
+	'id_column=s'       => \$id_column,
+	'gene_to_protein=s' => \$gene_to_protein,
+	'id_type=s'         => \$id_type,
+	'uniprot_db=s'     => \$uniprot_db,
 );
-my $annotation = GeneAnnotator->new(id_type => $id_type, uniprot => $uniprot, uniprot_dir=> $uniprot_dir );
-my $types = ['description','function', 'catalytic activity', 'tissue specificity', 'pathway', 'disease'];
+my $annotation = GeneAnnotator->new(
+	id_type         => $id_type,
+	gene_to_protein => $gene_to_protein,
+	uniprot_db      => $uniprot_db
+);
+my $ann_header = $annotation->get_header();
 open IN, $in or die "Can't open $in\n";
-if(!$skip_header){
-	my $header = <IN>;
-	chomp $header;
-	my @to_print = ($header, @$types);
-	print join("\t", @to_print), "\n";		
-}
-while(<IN>){
+my $header = <IN>;
+chomp $header;
+print "$header\t$ann_header\n";
+
+while (<IN>) {
 	chomp;
-	my @d = split /\t/;
-	my $id = $d[$id_column];
-	my $info = $annotation->protein_info($id, $types);
-	my @to_print = ($_, @$info);
-	print join("\t", @to_print), "\n";	
+	my @d        = split /\t/;
+	my $id       = $d[$id_column];
+	my $info     = $annotation->protein_info( $id);
+	my @to_print = ( $_, $info );
+	print join( "\t", @to_print ), "\n";
 }
 close IN;
 

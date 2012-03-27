9aaa3c7faee3a0de0d9e37818c6a4759eef3515b
reshetovdenis <reshetovdenis@6417c1ee-9a8a-29c9-8368-fd6fd0c9e1d9>
2012-03-27 11:14:16 +0000

    Towards fixing pindel config

--- trunk/accessory/pindel_config.pl
+++ trunk/accessory/pindel_config.pl
@@ -26,7 +26,7 @@ my $mean_rounded = sprintf "%.0f", $mean;
 open OUT, ">$out" or die "Can't open $out\n";
 
 #/data/results/Denis/S000006.20111030/S000006.20111030.bam.dedup.bam     342     S000006
-print OUT join( "\t", ( $bam, $mean_rounded, $id ) );
+print OUT join( "\t", ( $bam, $mean_rounded, $id ) ), "\n";
 close OUT;
 
 sub average {

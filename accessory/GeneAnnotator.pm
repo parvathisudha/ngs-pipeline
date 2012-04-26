8f08bf6aa0a4168f44aa9031c456b15054fe3a99
reshetovdenis <reshetovdenis@6417c1ee-9a8a-29c9-8368-fd6fd0c9e1d9>
2012-04-26 10:02:16 +0000

    New report functionality: it can add loci of interest info

--- accessory/GeneAnnotator.pm
+++ accessory/GeneAnnotator.pm
@@ -17,7 +17,7 @@ sub new {
 		$self->{$self->{id_type}} = $self->read_annotation( 1, 2, $self->{gene_to_protein} );
 	}
 	$self->{DB} = $self->read_uniprot_db;
-	$self->{empty} = "\t" x 7;
+	$self->{empty} = "\t" x 6;
 	return $self;
 }
 

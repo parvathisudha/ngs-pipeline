7630dd6840ef16f1fd5a5d7c39168dd467eb4b7e
reshetovdenis <reshetovdenis@6417c1ee-9a8a-29c9-8368-fd6fd0c9e1d9>
2012-06-21 11:36:32 +0000

    Working around Pindel implementation

--- accessory/coding_report.pl
+++ accessory/coding_report.pl
@@ -6,10 +6,11 @@ use Data::Dumper;
 my $excel_string_length_limit = 32759;
 
 ####### get arguments      ###
-my ($in, $out);
-GetOptions( 'in=s' => \$in, 
-			'out=s' => \$out, 
-			);
+my ( $in, $out );
+GetOptions(
+	'in=s'  => \$in,
+	'out=s' => \$out,
+);
 
 my $vcf = Vcf->new( file => $in );
 $vcf->parse_header( silent => 1 );
@@ -18,21 +19,23 @@ my $vep_format = $1
   if $header =~
   m/Description=\"Consequence type as predicted by VEP. Format: (.+?)\"/;
 my @vep_format = split( '\|', $vep_format );
-my @blank_result = map {""} @vep_format;
+my @blank_result = map { "" } @vep_format;
 
 # Do some simple parsing. Most thorough but slowest way how to get the data.
 my @result_header = (
 	'CHROM',               'POS',
 	'ID',                  'REF',
 	'ALT',                 'AF',
+	'PROGRAM',             'SVTYPE',
+	'SVLEN',               'END',
 	'CGI_FREQ',            'KG_FREQ',
 	'EUR_FREQ',            'FILTER',
 	'SNPEFF_EFFECT',       'SNPEFF_FUNCTIONAL_CLASS',
 	'SNPEFF_GENE_BIOTYPE', 'SNPEFF_GENE_NAME',
 	'SNPEFF_IMPACT',       'SNPEFF_TRANSCRIPT_ID',
 	'SNPEFF_CODON_CHANGE', 'SNPEFF_AMINO_ACID_CHANGE',
-	'SNPEFF_EXON_ID',      'SET', 'CONTROL',
-	@vep_format
+	'SNPEFF_EXON_ID',      'SET',
+	'CONTROL',             @vep_format
 );
 
 my $num_vep_format = scalar @vep_format;
@@ -43,46 +46,49 @@ print OUT join( "\t", @result_header ), "\n";
 my $exon_num   = get_feature_num_by_title( \@vep_format, 'EXON' );
 my $intron_num = get_feature_num_by_title( \@vep_format, 'INTRON' );
 while ( my $x = $vcf->next_data_hash() ) {
-	my $ref = safe_excel_string( [$x->{'REF'}]);
-	my $alt_alleles = safe_excel_string( $x->{'ALT'});
-	my $filter      = join( ',',  @{ $x->{'FILTER'} } );
+	my $ref         = safe_excel_string( [ $x->{'REF'} ] );
+	my $alt_alleles = safe_excel_string( $x->{'ALT'} );
+	my $filter      = join( ',', @{ $x->{'FILTER'} } );
 	my @csq         = split( ",", $x->{'INFO'}->{'CSQ'} );
 ###INFO=<ID=CSQ,Number=.,Type=String,Description="Consequence type as predicted by VEP. Format: Allele|Gene|Feature|Feature_type|Consequence|cDNA_position|CDS_position|Protein_position|Amino_acids|Codons|Existing_variation|PolyPhen|SIFT|CANONICAL|EXON|INTRON|CCDS">
 	my @first_to_print = (
-			$x->{'CHROM'},
-			$x->{'POS'},
-			$x->{'ID'},
-			$ref,
-			$alt_alleles,
-			$x->{'INFO'}->{'AF'},
-			$x->{'INFO'}->{'set'},
-			$x->{'INFO'}->{'CGI_FREQ.AF'},
-			$x->{'INFO'}->{'KG_FREQ.AF'},
-			$x->{'INFO'}->{'EUR_FREQ.AF'},
-			$filter,
-			$x->{'INFO'}->{'SNPEFF_EFFECT'},
-			$x->{'INFO'}->{'SNPEFF_FUNCTIONAL_CLASS'},
-			$x->{'INFO'}->{'SNPEFF_GENE_BIOTYPE'},
-			$x->{'INFO'}->{'SNPEFF_GENE_NAME'},
-			$x->{'INFO'}->{'SNPEFF_IMPACT'},
-			$x->{'INFO'}->{'SNPEFF_TRANSCRIPT_ID'},
-			$x->{'INFO'}->{'SNPEFF_CODON_CHANGE'},
-			$x->{'INFO'}->{'SNPEFF_AMINO_ACID_CHANGE'},
-			$x->{'INFO'}->{'SNPEFF_EXON_ID'},
-			$x->{'INFO'}->{'SET.set'},
-			$x->{'INFO'}->{'CONTROL.set'},
-		);
+		$x->{'CHROM'},
+		$x->{'POS'},
+		$x->{'ID'},
+		$ref,
+		$alt_alleles,
+		$x->{'INFO'}->{'AF'},
+		$x->{'INFO'}->{'set'},
+		$x->{'INFO'}->{'SVTYPE'},
+		$x->{'INFO'}->{'SVLEN'},
+		$x->{'INFO'}->{'END'},
+		$x->{'INFO'}->{'CGI_FREQ.AF'},
+		$x->{'INFO'}->{'KG_FREQ.AF'},
+		$x->{'INFO'}->{'EUR_FREQ.AF'},
+		$filter,
+		$x->{'INFO'}->{'SNPEFF_EFFECT'},
+		$x->{'INFO'}->{'SNPEFF_FUNCTIONAL_CLASS'},
+		$x->{'INFO'}->{'SNPEFF_GENE_BIOTYPE'},
+		$x->{'INFO'}->{'SNPEFF_GENE_NAME'},
+		$x->{'INFO'}->{'SNPEFF_IMPACT'},
+		$x->{'INFO'}->{'SNPEFF_TRANSCRIPT_ID'},
+		$x->{'INFO'}->{'SNPEFF_CODON_CHANGE'},
+		$x->{'INFO'}->{'SNPEFF_AMINO_ACID_CHANGE'},
+		$x->{'INFO'}->{'SNPEFF_EXON_ID'},
+		$x->{'INFO'}->{'SET.set'},
+		$x->{'INFO'}->{'CONTROL.set'},
+	);
 	for my $csq (@csq) {
 		my @vep_effect = split( '\|', $csq );
 		$vep_effect[$exon_num]   =~ s/\//|/;
 		$vep_effect[$intron_num] =~ s/\//|/;
-		if (scalar @vep_effect == $num_vep_format - 1 ){
-			push(@vep_effect, "");
+		if ( scalar @vep_effect == $num_vep_format - 1 ) {
+			push( @vep_effect, "" );
 		}
-		print OUT join( "\t", (@first_to_print, @vep_effect)), "\n";
+		print OUT join( "\t", ( @first_to_print, @vep_effect ) ), "\n";
 	}
-	unless(scalar @csq){
-		print OUT join( "\t", (@first_to_print, @blank_result)), "\n";
+	unless ( scalar @csq ) {
+		print OUT join( "\t", ( @first_to_print, @blank_result ) ), "\n";
 	}
 }
 
@@ -97,13 +103,14 @@ sub get_feature_num_by_title {
 	}
 	return $i;
 }
-sub safe_excel_string{
+
+sub safe_excel_string {
 	my ($array) = @_;
-	my $result_string = join( ',',  @{ $array } );
-	if(length $result_string >= $excel_string_length_limit){
+	my $result_string = join( ',', @{$array} );
+	if ( length $result_string >= $excel_string_length_limit ) {
 		my $i = 0;
-		$result_string = join( ',',  map {$i++; "LONG_ALLELE$i"} @{ $array } );
-	}	
+		$result_string = join( ',', map { $i++; "LONG_ALLELE$i" } @{$array} );
+	}
 	return $result_string;
 }
 

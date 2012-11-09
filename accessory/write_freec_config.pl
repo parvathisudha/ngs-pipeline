2b5b1236b27d9cb1692a359d0127cbc278301722
reshetovdenis <reshetovdenis@6417c1ee-9a8a-29c9-8368-fd6fd0c9e1d9>
2012-11-09 06:11:59 +0000

    Towards adding FREEC

--- accessory/write_freec_config.pl
+++ accessory/write_freec_config.pl
@@ -1,17 +1,18 @@
+#!/usr/bin/perl
 use strict;
 use Getopt::Long;
 use Data::Dumper;
 
 my ($out, $chrLenFile, $chrLenFile,$coefficientOfVariation,$ploidy,$outputDir,$GCcontentProfile,$mateFile, $gemMappabilityFile);
 GetOptions( 
-            'out' => \$out,
-            'chrLenFile' => \$chrLenFile,
-            'gemMappabilityFile' => \$gemMappabilityFile,
-            'coefficientOfVariation' => \$coefficientOfVariation,
-            'ploidy' => \$ploidy,
-            'outputDir' => \$outputDir,
-            'GCcontentProfile' => \$GCcontentProfile,
-            'mateFile' => \$mateFile,
+            'out=s' => \$out,
+            'chrLenFile=s' => \$chrLenFile,
+            'gemMappabilityFile=s' => \$gemMappabilityFile,
+            'coefficientOfVariation=s' => \$coefficientOfVariation,
+            'ploidy=s' => \$ploidy,
+            'outputDir=s' => \$outputDir,
+            'GCcontentProfile=s' => \$GCcontentProfile,
+            'mateFile=s' => \$mateFile,
         );
         
 my $config = <<CONF;

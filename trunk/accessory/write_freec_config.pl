c5b05e4407b53edcba2eea6b78dd4b572ed4328c
reshetovdenis <reshetovdenis@6417c1ee-9a8a-29c9-8368-fd6fd0c9e1d9>
2012-11-09 05:36:33 +0000

    Towards adding FREEC

---
+++ trunk/accessory/write_freec_config.pl
@@ -0,0 +1,37 @@
+use strict;
+use Getopt::Long;
+use Data::Dumper;
+
+my ($out, $chrLenFile, $chrLenFile,$coefficientOfVariation,$ploidy,$outputDir,$GCcontentProfile,$mateFile, $gemMappabilityFile);
+GetOptions( 
+            'out=s' => \$out,
+            'chrLenFile=s' => \$chrLenFile,
+            'gemMappabilityFile=s' => \$gemMappabilityFile,
+            'coefficientOfVariation=s' => \$coefficientOfVariation,
+            'ploidy=s' => \$ploidy,
+            'outputDir=s' => \$outputDir,
+            'GCcontentProfile=s' => \$GCcontentProfile,
+            'mateFile=s' => \$mateFile,
+        );
+        
+my $config = <<CONF;
+[general]
+
+chrLenFile = $chrLenFile
+gemMappabilityFile = $gemMappabilityFile
+coefficientOfVariation = $coefficientOfVariation
+ploidy = $ploidy
+GCcontentProfile = $GCcontentProfile
+outputDir = $outputDir
+
+
+[sample]
+mateFile = $mateFile
+inputFormat = BAM
+mateOrientation = FR
+
+CONF
+
+open OUT, ">$out";
+print OUT 
+close OUT;
